dc9992ed96259eac1164936bb61f6042213326f2
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-11-19 13:02:24 +0800

    check auth pass

--- android/src/main/java/com/ariasaproject/poolminerlite/MainActivity.java
+++ android/src/main/java/com/ariasaproject/poolminerlite/MainActivity.java
@@ -385,18 +385,21 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
     // button function
     public void toStartMining(View v) {
         updateState(MINE_STATE_ONSTART);
-        String[] dats = new String[3];
+        String[] dats = new String[4];
         dats[0] = sb.append(et_serv.getText()).toString();
         sb.setLength(0);
         dats[1] = sb.append(et_user.getText()).toString();
         sb.setLength(0);
         dats[2] = sb.append(et_pass.getText()).toString();
         sb.setLength(0);
+        dats[3] = "";
 
-        int[] dati = new int[2];
+        int[] dati = new int[4];
         dati[0] = Integer.parseInt(sb.append(et_port.getText()).toString());
         sb.setLength(0);
         dati[1] = sb_cpu.getProgress();
+        dati[2] = 0;
+        dati[3] = 0;
 
         tv_showInput.setText(
                 String.format(

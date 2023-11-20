9633d1c86f0a52bf63c8a40bae7a028f77dd3b44
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-11-20 09:00:07 +0800

    test new objective json

--- core/main/util.cpp
+++ core/main/util.cpp
@@ -9,17 +9,17 @@ void convert::hexString_toBiner (void *, const std::string, const size_t) {}
 //parsing json foward end
 
 struct minerpool::mine_data {
-	std
+	size_t x;
 };
 minerpool::mine_data minerpool::create_data() {
-	
+	return minerpool::minerdata();
 }
 bool minerpool::mine_data_update(mine_data*, const char*) {
-	
+	return false;
 }
 std::string minerpool::mine_data_extract(mine_data*) {
 	return std::string(R"(
-		
+		nothing
 		)");
 }
 

af7fada265791056dc0a945a8a9c8f010b254862
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-11-20 11:21:32 +0800

    check string input pass

--- android/src/main/cpp/MinerService.cpp
+++ android/src/main/cpp/MinerService.cpp
@@ -256,7 +256,7 @@ JNIF (void, nativeStart) (JNIEnv *env, jobject o, jobjectArray s, jintArray i) {
     jstring jserverName = (jstring)env->GetObjectArrayElement (s, 0);
     jsize len = env->GetStringUTFLength (jserverName);
     cd->server = new char[len];
-    const jchar *serverName = env->GetStringUTFChars (jserverName, JNI_FALSE);
+    const char *serverName = env->GetStringUTFChars (jserverName, JNI_FALSE);
     memcpy (cd->server, serverName, len);
     env->ReleaseStringChars (jserverName, serverName);
   }
@@ -264,7 +264,7 @@ JNIF (void, nativeStart) (JNIEnv *env, jobject o, jobjectArray s, jintArray i) {
     jstring jauth_user = (jstring)env->GetObjectArrayElement (s, 1);
     jsize len = env->GetStringUTFLength (jauth_user);
     cd->auth_user = new char[len];
-    const jchar *auth_user = env->GetStringUTFChars (jauth_user, JNI_FALSE);
+    const char *auth_user = env->GetStringUTFChars (jauth_user, JNI_FALSE);
     memcpy (cd->auth_user, auth_user, len);
     env->ReleaseStringChars (jauth_user, auth_user);
   }
@@ -272,7 +272,7 @@ JNIF (void, nativeStart) (JNIEnv *env, jobject o, jobjectArray s, jintArray i) {
     jstring jauth_pass = (jstring)env->GetObjectArrayElement (s, 2);
     jsize len = env->GetStringUTFLength (jauth_pass);
     cd->auth_pass = new char[len];
-    const jchar *auth_pass = env->GetStringUTFChars (jauth_pass, JNI_FALSE);
+    const char *auth_pass = env->GetStringUTFChars (jauth_pass, JNI_FALSE);
     memcpy (cd->auth_pass, auth_pass, len);
     env->CallVoidMethod (o, sendMessageConsole, 1, jauth_pass);
     env->CallVoidMethod (o, sendMessageConsole, 1, env->NewStringUTF (auth_pass));

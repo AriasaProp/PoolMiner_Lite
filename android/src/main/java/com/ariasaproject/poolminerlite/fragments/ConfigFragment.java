df0ecd1a6ec0671ae526c568346de23ca6f92cc6
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-06-03 07:37:55 +0800

    fixing in 03/06/2024

--- android/src/main/java/com/ariasaproject/poolminerlite/fragments/ConfigFragment.java
+++ android/src/main/java/com/ariasaproject/poolminerlite/fragments/ConfigFragment.java
@@ -6,6 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ariasaproject.poolminerlite.R;
+
 public class ConfigFragment extends Fragment {
 
     @Override

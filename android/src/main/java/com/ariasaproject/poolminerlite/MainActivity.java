2dd3a2a802f47eb9195d57541a94b63d3ff9ad84
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-06 17:56:04 +0800

    native-tab : 17:56:04 10/06/24

--- android/src/main/java/com/ariasaproject/poolminerlite/MainActivity.java
+++ android/src/main/java/com/ariasaproject/poolminerlite/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ariasaproject.poolminerlite;
 
+import static androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_DRAGGING;
+import static androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_IDLE;
+import static androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_SETTLING;
 
 import android.content.Context;
 import android.content.Intent;
@@ -7,13 +10,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
-import android.widget.FrameLayout;
 import android.widget.RadioGroup;
+import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatRadioButton;
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.AppCompatRadioButton;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
@@ -62,18 +68,16 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(pagerAdapter);
         final View tab_indc = findViewById(R.id.tabIndicator);
         final FrameLayout rootTab = findViewById(R.id.rootTabLayout);
-
-        tabs.setOnCheckedChangeListener(
-                (o, id) -> {
-                    AppCompatRadioButton vid = o.findViewById(id);
-                    int[] radioButtonLocation = new int[2];
-                    vid.getLocationOnScreen(radioButtonLocation);
-                    FrameLayout.LayoutParams p =
-                            new FrameLayout.LayoutParams(vid.getWidth(), vid.getHeight());
-                    p.leftMargin = radioButtonLocation[0] - rootTab.getLeft();
-                    p.topMargin = radioButtonLocation[1] - rootTab.getTop();
-                    tab_indc.setLayoutParams(p);
-                });
+        
+        tabs.setOnCheckedChangeListener((o, id) -> {
+        	AppCompatRadioButton vid = o.findViewById(id);
+	        int[] radioButtonLocation = new int[2];
+	        vid.getLocationOnScreen(radioButtonLocation);
+	        FrameLayout.LayoutParams p = new FrameLayout.LayoutParams(vid.getWidth(),vid.getHeight());
+	        p.leftMargin = radioButtonLocation[0] - rootTab.getLeft();
+	        p.topMargin = radioButtonLocation[1] - rootTab.getTop();
+	        tab_indc.setLayoutParams(p);
+        });
         // set default
         final AppCompatRadioButton def = findViewById(R.id.radio_miner);
         def.setChecked(true);

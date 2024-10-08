f855e6faf1460f06a109dc23618e88b22b31849f
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-08 20:08:34 +0800

    backto_cpp : 20:08:33 10/08/24

--- android/src/main/cpp/MinerService.cpp
+++ android/src/main/cpp/MinerService.cpp
@@ -17,9 +17,6 @@
 #include <utility>
 #include <vector>
 
-#include "json.hpp"
-#include "util.hpp"
-
 #define STATE_NONE 0
 #define STATE_ONSTART 1
 #define STATE_RUNNING 2
@@ -74,158 +71,6 @@ void MinerService_OnUnload (JNIEnv *env) {
   sendMessageConsole = NULL;
 }
 
-// mine data holder
-struct mining_notify_data {
-  std::string job_id;
-  hex_array version;
-  std::vector<hex_array> merkle_arr;
-  hex_array ntime;
-  hex_array nbit;
-  bool clean;
-  hex_array prev_hash;
-  hex_array coinb1;
-  hex_array coinb2;
-  hex_array xnonce2;
-};
-struct mine_data_holder {
-private:
-	hex_array session_id;
-	hex_array difficulty_;
-	double difficulty;
-	hex_array xnonce1;
-	std::string xnonce2_size = "not set";
-	std::string version = "not set";
-	mining_notify_data mnd;
-public:
-  std::vector<std::string> json_list_a;
-	bool subscribed = false;
-	bool authorized = false;
-	
-	void updateData(json::JSON d) {
-		std::string _h;
-		//throw error
-		if (d.hasKey("error") && !d["error"].IsNull())
-			_h = (std::string)d["error"];
-		//valid result
-		if (d.hasKey("id") && !d["id"].IsNull()) {
-			//statisfy any requested result
-			int id = d["id"];
-			switch (id) {
-				case 1: {
-					//is subscribe
-		      if (!d.hasKey("result") || d["result"].IsNull() || (d["result"].JSONType() != json::JSON::Class::Array)) throw std::runtime_error("hasn't valid result");
-					json::JSON &res = d["result"];
-					// method data extraction
-					//1
-					{
-						std::string method = res[0][0][0];
-						std::string value = res[0][0][1];
-						if (method == "mining.notify") {
-							session_id = value;
-						}
-						if (method == "mining.set_difficulty") {
-							difficulty_ = value;
-						}
-					}
-					//2
-					{
-						std::string method = res[0][1][0];
-						std::string value = res[0][1][1];
-						if (method == "mining.notify") {
-							session_id = value;
-						}
-						if (method == "mining.set_difficulty") {
-							difficulty_ = value;
-						}
-					}
-					// xnonce1
-					xnonce1 = res[1];
-					//xnonce2 size
-					xnonce2_size = (std::string)res[2];
-					subscribed = true;
-				} break;
-				case 2: {
-					//is authorize
-					if (!d.hasKey("result") || d["result"].IsNull() || !((bool)d["result"])) throw std::runtime_error("authorize is invalid");
-					authorized = true;
-				} break;
-				default:
-					_h = std::string("unhandled id ") + (std::string)d["id"];
-			}
-		} else if (d.hasKey("method") && d.hasKey("params")) {
-			//statisfy any received method
-			std::string method = d["method"];
-			if (method == "mining.set_difficulty") {
-				difficulty = d["params"][0];
-			} else if (method == "mining.notify") {
-				json::JSON params = d["params"];
-				if (params.size() < 8) throw std::runtime_error("mining.notify params has not enough informations!");
-		    mnd.job_id = (std::string)params[0];
-		    mnd.prev_hash = params[1];
-		    
-		    mnd.coinb1 = params[2];
-		    mnd.coinb2 = params[3];
-		    // merkle_arr
-		    {
-		  		json::JSON jm = params[4];
-		  		if (jm.JSONType() != json::JSON::Class::Array) throw std::runtime_error("merkle_array params is invalid!");
-		  		mnd.merkle_arr.clear();
-		  		mnd.merkle_arr.reserve(jm.size());
-		  		for (auto it = jm.ArrayRange().begin(); it < jm.ArrayRange().end(); ++it) {
-		  			mnd.merkle_arr.push_back((std::string)*it);
-		  		}
-		    }
-		    mnd.version = params[5];
-		    mnd.nbit = params[6];
-		    mnd.ntime = params[7];
-		    mnd.clean = params[8];
-			} else if ((method == "client.get_version") && d.hasKey("jsonrpc") && d["jsonrpc"].IsNull()) {
-	      version = (std::string)d["jsonrpc"];
-			} else {
-				_h = std::string("unhandled method: ") + method;
-			}
-		} else {
-			_h = d.dump(1, " ");
-		}
-		
-		if (_h.empty()) {
-			json_list_a.push_back(_h);
-		}
-	}
-/*
-	void doJob() {
-		make header
-		
-        // Increment extranonce2
-        HexArray xnonce2 = this._xnonce2;
-        String xnonce2_str = xnonce2.getStr();
-        for (int i = 0; i < xnonce2.getLength() && (0 == (++xnonce2.refHex()[i])); i++)
-            ;
-
-        // Assemble block header
-        HexArray work_data = new HexArray(this._notify.version);
-        work_data.append(this._notify.prev_hash);
-        work_data.append(this._merkle_loot, 0, 32);
-        work_data.append(this._notify.ntime);
-        work_data.append(this._notify.nbit);
-        work_data.append(new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte) 0x80});
-        work_data.append(new byte[40]);
-        work_data.append(new byte[] {(byte) 0x80, 0x02, 0x00, 0x00});
-        return new StratumMiningWork(
-                work_data,
-                diff2target(this._difficulty / 65536.0),
-                this._notify.job_id,
-                xnonce2_str);
-    // Increment extranonce2
-    
-    // Assemble block header
-    std::string header = mnd.version;
-    std::string prev = mnd.prev_hash;
-    header += prev;
-	}
-		*/
-};
-
 // 5 kBytes ~> 40 kBit
 #define MAX_MESSAGE 5000
 #define CONNECT_MACHINE "PoolMiner-Lite"
@@ -236,30 +81,6 @@ struct connectData {
   char *auth_pass;
 };
 
-void *doWork (void *p) {
-  const uint32_t start = static_cast<uint32_t> ((unsigned long)p);
-  uint32_t nonce = start;
-  pthread_mutex_lock (&_mtx);
-  ++active_worker;
-  pthread_mutex_unlock (&_mtx);
-  bool loop;
-  do {
-    sleep (1);
-    // here hashing
-    pthread_mutex_lock (&_mtx);
-    loop = doingjob;
-    uint32_t nn = nonce + thread_use;
-    pthread_mutex_unlock (&_mtx);
-    if (nn < nonce) break;
-    nonce = nn;
-    // do hashing
-  } while (loop);
-  pthread_mutex_lock (&_mtx);
-  --active_worker;
-  pthread_cond_broadcast (&_cond);
-  pthread_mutex_unlock (&_mtx);
-  pthread_exit (NULL);
-}
 char buffer[MAX_MESSAGE];
 void *startConnect (void *p) {
   pthread_mutex_lock (&_mtx);
@@ -267,7 +88,6 @@ void *startConnect (void *p) {
   pthread_mutex_unlock (&_mtx);
 
   connectData *dat = (connectData *)p;
-  mine_data_holder mdh;
 	//make socket
   try {
 	  int sockfd = socket (AF_INET, SOCK_STREAM, 0);
@@ -281,15 +101,6 @@ void *startConnect (void *p) {
         sleep (1);
       }
 	    // try subscribe & authorize
-	    {
-	      JNIEnv *env;
-	      if (global_jvm->AttachCurrentThread (&env, &attachArgs) == JNI_OK) {
-	        env->CallVoidMethod (local_globalRef, sendMessageConsole, 2, env->NewStringUTF ("start sending"));
-	        env->CallVoidMethod (local_globalRef, sendMessageConsole, 2, env->NewStringUTF (dat->auth_user));
-	        env->CallVoidMethod (local_globalRef, sendMessageConsole, 2, env->NewStringUTF (dat->auth_pass));
-	        global_jvm->DetachCurrentThread ();
-	      }
-	    }
     	snprintf(buffer, MAX_MESSAGE,"{\"id\":1,\"method\":\"mining.subscribe\",\"params\":[\"%s\"]}\n{\"id\":2,\"method\":\"mining.authorize\",\"params\":[\"%s\",\"%s\"]}", CONNECT_MACHINE, dat->auth_user, dat->auth_pass);
       tries = 0;
       for (size_t length = strlen(buffer), s; length > 0;) {
@@ -301,13 +112,7 @@ void *startConnect (void *p) {
 		    length -= s;
 		    memmove(buffer, buffer + s, length);
       }
-	    {
-	      JNIEnv *env;
-	      if (global_jvm->AttachCurrentThread (&env, &attachArgs) == JNI_OK) {
-	        env->CallVoidMethod (local_globalRef, sendMessageConsole, 2, env->NewStringUTF ("Sended"));
-	        global_jvm->DetachCurrentThread ();
-	      }
-	    }
+/*
 	    // recv subscribe & authorize prove
 	    tries = 0;
 	    do {
@@ -333,6 +138,7 @@ void *startConnect (void *p) {
 	        global_jvm->DetachCurrentThread ();
 	      }
 	    }
+*/
 	    // loop update data from server
 	    tries = 0;
 	    {
@@ -351,13 +157,10 @@ void *startConnect (void *p) {
 	          sleep (1);
 	        } else {
 	          if (tries) tries = 0;
-	          std::string msgRcv (buffer);
-	          size_t pos = 0;
-	          while ((pos = msgRcv.find ("\n")) != std::string::npos) {
-	            json::JSON rcv = json::Parse (msgRcv.substr (0, pos));
-	            msgRcv.erase (0, pos + 1);
-	            if (rcv.IsNull ()) continue;
-	            mdh.updateData (rcv);
+            JNIEnv *env;
+	          if (global_jvm->AttachCurrentThread (&env, &attachArgs) == JNI_OK) {
+	            env->CallVoidMethod (local_globalRef, sendMessageConsole, 0, env->NewStringUTF (buffer));
+	            global_jvm->DetachCurrentThread ();
 	          }
 	        }
 	      }
@@ -385,15 +188,6 @@ void *startConnect (void *p) {
     JNIEnv *env;
 	  if (global_jvm->AttachCurrentThread (&env, &attachArgs) == JNI_OK) {
 	    env->CallVoidMethod (local_globalRef, updateState, STATE_NONE);
-			if (!mdh.json_list_a.empty()) {
-				strcpy(buffer, "Result: \n");
-				for (std::string erl : mdh.json_list_a) {
-					strcat(buffer, erl.c_str());
-					strcat(buffer, "\n");
-				}
-				env->CallVoidMethod (local_globalRef, sendMessageConsole, 0, env->NewStringUTF(buffer));
-	  		mdh.json_list_a.clear();
-			}
 		  global_jvm->DetachCurrentThread ();
 	  }
   }
